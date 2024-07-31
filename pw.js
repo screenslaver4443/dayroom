8c84a4332bb00588ed81c5317a8d8a5ba67a0012
Nikolai <57935446+screenslaver4443@users.noreply.github.com>
2024-07-31 20:35:49 +0930

    completed playwright code for extracting data from daymap (will need fixes for school wifi), updated gitignore

--- pw.js
+++ pw.js
@@ -3,12 +3,12 @@ const cheerio = require('cheerio')
 const dotenv = require('dotenv')
 dotenv.config()
 
-async function main(dmapusername, dmappassword) {
+async function daymapScrape(dmapusername, dmappassword) {
     async function login(username, password) {
         let title = await page.title();
         if (title === 'EdPass Service') {
             await page.click('tr[data-idp-name="glenungainternationalhighschool"]');
-            // await page.click('span[class="continue_button button"]');
+            await page.click('span[class="continue_button button"]');
             await page.waitForLoadState('load')
             await page.fill('input[name="username"]', username);
             await page.fill('input[name="password"]', password);
@@ -19,28 +19,52 @@ async function main(dmapusername, dmappassword) {
             await page.fill('input[name="password"]', password);
             await page.click('input[id="okta-signin-submit"]');
         }
-        // else {
-        //     throw new Error('Unknown page '+title);
-        // }
+        else {
+            throw new Error('Unknown page: '+title);
         }
-    const browser = await playwright.chromium.launch({headless: false});
+        }
+    async function extractTaskData() {
+        await page.waitForSelector('table[class="rgMasterTable"]');
+        let dtaskstble = [];
+        await page.waitForLoadState('load');
+        let html = await page.content();
+
+        const $ = cheerio.load(html);
+        const tablebody = $('table.rgMasterTable').children('tbody');
+        tablebody.find('tr').each((index, element) => {
+            if ($(element).hasClass('rgGroupHeader')) {
+                return; // Skip headers
+            }
+            let items = [];
+            $(element).find('td').each((index, element) => {
+                items.push($(element).text());
+            });
+            dtaskstble.push(items);
+        });
+        // for loop to convert the array into objects 
+        for (let i = 0; i < dtaskstble.length; i++) {
+            let obj = {};
+            obj['subject'] = dtaskstble[i][2];
+            obj['task'] = dtaskstble[i][4];
+            obj['due'] = dtaskstble[i][6];
+            obj['assesment'] = dtaskstble[i][8];
+            obj['status'] = dtaskstble[i][9];
+            dtaskstble[i] = obj;
+        }
+        return dtaskstble;
+    }
+    const browser = await playwright.chromium.launch({headless: true});
 
     const page = await browser.newPage();
     await page.goto('https://gihs.daymap.net/daymap/student/assignments.aspx');
-    await page.waitForEvent('load');
+    // await page.waitForEvent('load');
     await login(dmapusername, dmappassword);
-    console.log('logged in')
-    // await page.waitForSelector('table[class="rgMasterTable"]')
-    // let dtaskstble = []
-    // await page.waitForLoadState('load')
-    // let html = await page.content();
-
-    // const ch = cheerio.load(html);
-    // dtaskstble.push(ch('tr.rgRow').text())
-    // console.log(dtaskstble)
-
+    var dtaskstble = await extractTaskData();
     await browser.close();
+    return dtaskstble;
 }
 
 //Test
-main(process.env["DM_username"], process.env["DM_password"]);
+(async () => {
+    console.log(await daymapScrape(process.env["DM_username"], process.env["DM_password"]))
+})()
